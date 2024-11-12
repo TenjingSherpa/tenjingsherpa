340632fab96facedf6f87fe71d4a0302aeeee077
Tenjing Sherpa <100190519+TenjingSherpa@users.noreply.github.com>
2024-11-12 14:07:45 +0100

    Delete Jekyll directory

--- Jekyll/javascript.js
+++
@@ -1,9 +0,0 @@
-const counterDisplay = document.getElementById('counter');
-const incrementButton = document.getElementById('incrementButton');
-
-let counter = 0;
-
-incrementButton.addEventListener('click', () => {
-  counter += 1;
-  counterDisplay.textContent = counter;
-});
